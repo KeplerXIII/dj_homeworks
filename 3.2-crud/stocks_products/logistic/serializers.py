7d6d9214f50351a1a6b0be8f0bee46bf0c22e617
a.pushkin <a.v.pushkin@gmail.com>
2023-01-23 16:43:01 +0300

    3.2 initial

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,31 +1,38 @@
 from rest_framework import serializers
+from logistic.models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['id', 'product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
-
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
+        print(positions)
         # создаем склад по его параметрам
         stock = super().create(validated_data)
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+
 
         return stock
 
